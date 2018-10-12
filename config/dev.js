639b50c25b1502d86cbb71a4f67641963d21bcec
kcuellar <katherine.cuellar93@gmail.com>
2018-10-11 20:15:34 -0400

    initial commit for Passport

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev.js DON'T COMMIT THIS
+
+module.exports={
+    cookieKey: 'alkfawovinawetha295sfhiuw48fhg',
+    googleClientID: '551734598360-mtc38ti86h3b5pcfr59kp77el7vgl4n7.apps.googleusercontent.com',
+    googleClientSecret: 'BVEtlUfNtwSFGBMIWLqhGtbd',
+}

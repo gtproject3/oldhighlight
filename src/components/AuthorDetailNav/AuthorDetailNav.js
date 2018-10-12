05a90e9f01823bbb0a0911c11842e34963c0f50e
1661sons <julia.kerouac@gmail.com>
2018-10-11 21:53:39 -0400

    adding individual components

---
+++ src/components/AuthorDetailNav/AuthorDetailNav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./AuthorDetailNav.css";
+
+// :TODO: props needs to be passed in somewhere, along with proper routes
+
+const AuthorDetailNav = props => (
+    <nav id="galaxyTop" className="uk-navbar-container uk-navbar-transparent" uk-navbar>
+        <div className="uk-navbar-left">
+            <ul className="uk-navbar-nav">
+                <li><a href="index.html"><span uk-icon="bolt"></span>&nbsp;highlight</a></li>
+                <li><a href="authorForm.html"><span uk-icon="plus"></span>&nbsp;add author</a></li>
+            </ul>
+        </div>
+        <div className="uk-navbar-right">
+            <ul className="uk-navbar-nav">
+                <li><button id="loginButton" className="uk-button uk-button-secondary">login</button></li>
+            </ul>
+        </div>
+    </nav>
+);
+
+export default AuthorDetailNav;

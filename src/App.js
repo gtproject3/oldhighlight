23e77dee116110220c294c3d2d81d64fe42b9823
Sarah Subero <ssubero06@gmail.com>
2018-10-13 11:46:46 -0400

    Made changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import AuthorCard from "./components/AuthorCard";
+import AuthorDetailNav from "./components/AuthorDetailNav";
+/*import AuthorForm from "./components/AuthorForm";*/
+import HomeNav from "./components/HomeNav";
+import SearchPanel from "./components/SearchPanel";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App"></div>
-    );
-  }
-}
+const App = () => (
+  <div className="container-fluid mainPage">
+  <AuthorDetailNav/>
+  <AuthorCard/>
+  <HomeNav/>
+  {/*<AuthorForm/>*/}
+  <SearchPanel/>
+
+    {/* you need to break down each part of the page into a component */ }
+    { /* from there, we'll link them as: <Sidebar/> , <NavBar/> etc.*/ }
+    { /* ask TA if this is the best way to do it. */} 
+  </div>
+);
 
 export default App;
+
+

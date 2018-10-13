f2de67b483e71e2812048c36ae16395efcaade5a
Annie Vallone <annievallone@gmail.com>
2018-10-13 11:31:02 -0400

    update for find many

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -8,6 +8,17 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
+    findMany: function () {
+        db.Author
+            .find(search)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
 
+
+    }
+
+    // Model.$where('this.firstname === this.lastname').exec(callback)
 };
 
+// gender: xx
+// race : xx
